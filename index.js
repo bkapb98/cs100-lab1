898bd23727920bec904495a05d6f046cdcbfb78f
Ben <bkaplan@college.harvard.edu>
2018-09-15 23:49:30 -0400

    progress, still need buttons images, links

--- index.js
+++ index.js
@@ -26,6 +26,51 @@ const showGalleries = (url) => {
 };
 
 const showObjectsTable = (id) => {
-  document.querySelector("#all-objects").style.display = "block";
+  const url = `https://api.harvardartmuseums.org/object?apikey=${API_KEY}&?gallery=${id}`;
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      var table = document.getElementById("myTable");
+      var row = table.insertRow();
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      var cell3 = row.insertCell(2);
+      var cell4 = row.insertCell(3);
+
+      cell1.innerHTML = 'Name',
+      cell2.innerHTML = 'People',
+      cell3.innerHTML = 'Image';
+      cell4.innerHTML = 'URL';
+
+        data.records.forEach(object => {
+          var row = table.insertRow();
+          var cell1 = row.insertCell(0);
+          var cell2 = row.insertCell(1);
+          var cell3 = row.insertCell(2);
+          var cell4 = row.insertCell(3);
+
+          cell1.innerHTML = object.title,
+          cell2.innerHTML = object.people,
+          cell3.innerHTML = object.images[0];
+          cell4.innerHTML = `<a href="#${object.id}" onclick="showObject(${object.id})"'>See More</a>`;
+          document.querySelector("#all-galleries").style.display = "none";
+          document.querySelector("#button2").style.display = "none";
+        });
+  })
+};
+
+const showObject = (id) => {
+  const url = `https://api.harvardartmuseums.org/?apikey=${API_KEY}&?object=${id}`;
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+  document.querySelector("#object").innerHTML += `
+    <li> title: ${object.title} </li>
+    </p> 
+  `;
   document.querySelector("#all-galleries").style.display = "none";
-}
+  document.querySelector("#all-objects").style.display = "none";
+  document.querySelector("#button1").style.display = "none";
+})};
