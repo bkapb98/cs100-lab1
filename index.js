cc93c6e69d9d2ebc899e0f7865ad6f2a4f630cce
Ben <bkaplan@college.harvard.edu>
2018-09-16 11:39:02 -0400

    added back buttons, and object view

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ document.addEventListener("DOMContentLoaded", () => {
   showGalleries(url);
 });
 
+// global variable for gallery holder so can go back webpages
+var globalGalleryHolder = null;
+
 const showGalleries = (url) => {
   fetch(url)
     .then(response => response.json())
     .then(data => {
       console.log(data);
+      document.querySelector("#all-galleries").style.display = "block";
+      document.querySelector("#all-objects").style.display = "none";
+      document.querySelector("#button1").style.display = "none";
+      document.querySelector("#button2").style.display = "none";
       data.records.forEach(gallery => {
         document.querySelector("#galleries").innerHTML += `
           <li>
@@ -26,11 +33,19 @@ const showGalleries = (url) => {
 };
 
 const showObjectsTable = (id) => {
-  const url = `https://api.harvardartmuseums.org/object?apikey=${API_KEY}&?gallery=${id}`;
+  globalGalleryHolder = id;
+  const url = 
+    `https://api.harvardartmuseums.org/object?apikey=${API_KEY}&gallery=${id}`;
   fetch(url)
     .then(response => response.json())
     .then(data => {
       console.log(data);
+      document.querySelector("#all-galleries").style.display = "none";
+      document.querySelector("#button1").style.display = "block";
+      var button = document.getElementById("button1");
+      button.addEventListener("click", () =>{
+        showGalleries()});
+      document.querySelector("#all-objects").style.display = "block";
       var table = document.getElementById("myTable");
       var row = table.insertRow();
       var cell1 = row.insertCell(0);
@@ -50,27 +65,43 @@ const showObjectsTable = (id) => {
           var cell3 = row.insertCell(2);
           var cell4 = row.insertCell(3);
 
-          cell1.innerHTML = object.title,
-          cell2.innerHTML = object.people,
-          cell3.innerHTML = object.images[0];
-          cell4.innerHTML = `<a href="#${object.id}" onclick="showObject(${object.id})"'>See More</a>`;
-          document.querySelector("#all-galleries").style.display = "none";
+          cell1.innerHTML = 
+          `<a href="#${object.objectnumber}" 
+              onclick="showObject(${object.objectnumber})"'>${object.title}
+          </a>`
+          cell2.innerHTML = object.people[0].name,
+          cell3.innerHTML = `<img src=${object.images[0]}>`;
+          cell4.innerHTML = `<a href="${object.url}">See More</a>`;
           document.querySelector("#button2").style.display = "none";
         });
   })
 };
 
-const showObject = (id) => {
-  const url = `https://api.harvardartmuseums.org/?apikey=${API_KEY}&?object=${id}`;
+const showObject = (number) => {
+  const url = `https://api.harvardartmuseums.org/object?apikey=${API_KEY}&objectnumber=${number}`;
   fetch(url)
     .then(response => response.json())
     .then(data => {
       console.log(data);
+  document.querySelector("#button2").style.display = "block";
   document.querySelector("#object").innerHTML += `
-    <li> title: ${object.title} </li>
-    </p> 
+    <li> Title: ${data.records[0].title} </li>
+    <li> Description: ${data.records[0].description} </li>
+    <li> Provenance: ${data.records[0].provenance} </li>
+    <li> Accession Year: ${data.records[0].accessionyear} </li>
+    <li> <img src = ${data.records[0].images[0]}> </li>
   `;
   document.querySelector("#all-galleries").style.display = "none";
   document.querySelector("#all-objects").style.display = "none";
   document.querySelector("#button1").style.display = "none";
 })};
+
+const toGallery = () => {
+  // const url = `https://api.harvardartmuseums.org/gallery?apikey=${API_KEY}`;
+  // showGalleries(url);
+  location.reload()
+};
+
+const toObjects = () => {
+  showObjectsTable(globalGalleryHolder);
+}
